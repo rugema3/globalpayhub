8da8ff2e205459c3a7c9487bfb78374dc4f942a3
rugema3 <rugema61@gmail.com>
2023-12-06 22:01:33 +0200

    pushing the recent changes. i have implemented a separate app1.py for testing the new end points that were recently changed. created the airtime module that contains the Airtime class. and made a few more tweeks.

---
+++ airtime.py
@@ -0,0 +1,153 @@
+import requests
+import json
+
+class Airtime:
+    """
+    A class for vending airtime using the provided API.
+    """
+
+    def __init__(self, base_url, access_token):
+        """
+        Initialize the Airtime instance.
+
+        Parameters:
+        - base_url (str): The base URL of the vending API.
+        - access_token (str): The access token used for authentication.
+        """
+        self.base_url = base_url
+        self.access_token = access_token
+
+    def perform_authenticated_request(self, url, method="GET", data=None):
+        """
+        Perform an authenticated request to the API.
+
+        Parameters:
+        - url (str): The URL for the request.
+        - method (str): The HTTP method (GET or POST).
+        - data (dict): The request payload for POST requests.
+
+        Returns:
+        - dict: The JSON response from the API.
+        """
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+
+        if method == "GET":
+            response = requests.get(url, headers=headers)
+        elif method == "POST":
+            response = requests.post(url, headers=headers, json=data)
+        else:
+            raise ValueError(f"Unsupported HTTP method: {method}")
+
+        return response.json()
+
+    def vend_validate(self, vertical_id, customer_account_number):
+        """
+        Perform vend validation.
+
+        Parameters:
+        - vertical_id (str): The service/product vertical unique identifier.
+        - customer_account_number (str): The account number for the customer.
+
+        Returns:
+        - dict: The JSON response from the vend validation.
+        """
+        url = f"{self.base_url}/vend/validate"
+        data = {
+            "verticalId": vertical_id,
+            "customerAccountNumber": customer_account_number,
+        }
+
+        return self.perform_authenticated_request(url, method="POST", data=data)
+
+    def vend_execute(self, trx_id, customer_account_number, amount, vertical_id, delivery_method, deliver_to, callback):
+        """
+        Perform vend execution.
+
+        Parameters:
+        - trx_id (str): The transaction ID from the vend validation response.
+        - customer_account_number (str): The account number for the customer.
+        - amount (float): The transaction amount.
+        - vertical_id (str): The service/product vertical unique identifier.
+        - delivery_method (str): The delivery method for the transaction.
+        - deliver_to (str): The delivery destination for the transaction.
+        - callback (str): The callback URL for asynchronous processing.
+
+        Returns:
+        - dict: The JSON response from the vend execution.
+        """
+        url = f"{self.base_url}/vend/execute"
+        data = {
+            "trxId": trx_id,
+            "customerAccountNumber": customer_account_number,
+            "amount": amount,
+            "verticalId": vertical_id,
+            "deliveryMethodId": delivery_method,
+            "deliverTo": deliver_to,
+            "callBack": callback,
+        }
+
+        return self.perform_authenticated_request(url, method="POST", data=data)
+
+    def convert_to_local_currency(self, usd_amount):
+        """
+        Converts the given USD amount to the local currency.
+
+        Parameters:
+            usd_amount (float): The amount in USD to be converted.
+
+        Returns:
+            float: The equivalent amount in the local currency.
+
+        Raises:
+            ValueError: If the conversion for the given USD amount is not found in the amount_list.
+        """
+        # Replace this with your actual conversion logic using the amount_list or any other method
+        amount_list = {
+            0.1: 100,
+            0.2: 220,
+            1.00: 1100,
+            5.00: 5500,
+            8.00: 88000,
+            10.00: 11000,
+            15.00: 16500,
+            20.00: 22000
+        }
+        local_amount = amount_list.get(usd_amount)
+        if local_amount is None:
+            raise ValueError(f"Amount for {usd_amount} USD not found.")
+
+        return local_amount
+
+if __name__ == "__main__":
+    # Replace with your actual values
+    base_url = "https://sb-api.efashe.com/rw/v2"
+    access_token = "access_token"
+
+    # Create an instance of Airtime
+    airtime = Airtime(base_url, access_token)
+
+    # Replace with your actual values
+    vertical_id = "airtime"
+    customer_account_number = "0781049931"
+
+    # Perform vend validation
+    validate_response = airtime.vend_validate(vertical_id, customer_account_number)
+    print("Validation Response:")
+    print(json.dumps(validate_response, indent=2))
+
+    # Extract the dynamic trxId from the validation response
+    trx_id = validate_response.get("data", {}).get("trxId", "")
+    delivery_method = validate_response.get("data", {}).get("deliveryMethods", [{}])[0].get("id", "")
+    deliver_to = validate_response.get("data", {}).get("deliverTo", "")
+    callback = validate_response.get("data", {}).get("callback", "")
+    amount = 200
+
+    # Perform vend execution with the dynamic trxId
+    execute_response = airtime.vend_execute(trx_id, customer_account_number, amount, vertical_id, delivery_method, deliver_to, callback)
+    print("Execution Response:")
+    print(json.dumps(execute_response, indent=2))
+
---
+++ app1.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request, redirect, session
+from airtime import Airtime
+from paypal_handler import PayPalHandler
+from credentials import PAYPAL_MODE, PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET
+import secrets
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)  # Generate a random secret key
+
+base_url = os.getenv("AIRTIME_BASE_URL")
+access_token = os.getenv("AIRTIME_ACCESS_TOKEN")
+vertical_id = "airtime"  
+
+# Create instances of Airtime and PayPalHandler
+airtime = Airtime(base_url, access_token)
+paypal_handler = PayPalHandler(PAYPAL_MODE, PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('initiate_transaction.html')
+
+# Initiate transaction route
+@app.route('/initiate_transaction', methods=['GET', 'POST'])
+def initiate_transaction():
+    if request.method == 'POST':
+        # Get the user's input from the form (you might want to add more validation)
+        customer_account_number = request.form['customer_account_number']
+        usd_amount = float(request.form['usd_amount'])
+
+        # Perform vend validation
+        validate_response = airtime.vend_validate(vertical_id, customer_account_number)
+
+        # Extract necessary information for PayPal payment
+        trx_id = validate_response.get("data", {}).get("trxId", "")
+        delivery_method = validate_response.get("data", {}).get("deliveryMethods", [{}])[0].get("id", "")
+        deliver_to = validate_response.get("data", {}).get("deliverTo", "")
+        callback = validate_response.get("data", {}).get("callback", "")
+
+        # Save necessary information in session for later use in the execute route
+        session['transaction_info'] = {
+            'trx_id': trx_id,
+            'customer_account_number': customer_account_number,
+            'usd_amount': usd_amount,
+            'vertical_id': vertical_id,
+            'delivery_method': delivery_method,
+            'deliver_to': deliver_to,
+            'callback': callback,
+        }
+
+        # Create a PayPal payment and get the redirect URL
+        paypal_redirect_url = paypal_handler.create_payment(usd_amount, customer_account_number, request)
+
+        # Redirect the user to PayPal for payment
+        return redirect(paypal_redirect_url)
+
+    # Render the initiate_transaction.html template for GET requests
+    return render_template('initiate_transaction.html')
+
+# Execute payment route
+@app.route('/execute_payment', methods=['GET', 'POST'])
+def execute_payment():
+    # Retrieve necessary information from session
+    transaction_info = session.get('transaction_info')
+
+    if transaction_info is None:
+        return render_template('error.html', error_message='Transaction information not found in session.')
+
+    # Extract necessary information for vend execution
+    trx_id = transaction_info['trx_id']
+    customer_account_number = transaction_info['customer_account_number']
+    usd_amount = transaction_info['usd_amount']
+    vertical_id = transaction_info['vertical_id']
+    delivery_method = transaction_info['delivery_method']
+    deliver_to = transaction_info['deliver_to']
+    callback = transaction_info['callback']
+
+    # Execute the PayPal payment
+    success, error_message = paypal_handler.execute_payment(request.args.get('paymentId'), request.args.get('PayerID'))
+
+    if success:
+        # Perform vend execution with the retrieved information
+        execute_response = airtime.vend_execute(trx_id, customer_account_number, usd_amount, vertical_id, delivery_method, deliver_to, callback)
+        return render_template('success.html', execute_response=execute_response)
+    else:
+        # Handle payment execution failure
+        return render_template('error.html', error_message=error_message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
